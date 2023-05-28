bb0c11e8614ce74cc73e6c83bc3b83ccd78aa5ed
Natsu-hub <128861583+Natsu-hub@users.noreply.github.com>
2023-05-28 17:09:14 +0900

    Add files via upload
    2023.5.28

---
+++ js/script.js
@@ -0,0 +1,55 @@
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+
+  //to-top
+  var topBtn = $('.js-to-top');
+  topBtn.hide();
+
+  // ボタンの表示設定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 70) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+  });
+
+  // ボタンをクリックしたらスクロールして上に戻る
+  topBtn.click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 300, 'swing');
+    return false;
+  });
+
+
+  //ドロワーメニュー
+  $('.js-hamburger').on('click', function () {
+    if ($('.js-hamburger').hasClass('is-open')) {
+      $('.js-drawer-menu').fadeOut();
+      $(this).removeClass('is-open');
+    } else {
+      $('.js-drawer-menu').fadeIn();
+      $(this).addClass('is-open');
+    }
+  });
+
+
+  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
+  $(document).on('click', 'a[href*="#"]', function () {
+    let time = 400;
+    let header = $('header').innerHeight();
+    let target = $(this.hash);
+    if (!target.length) return;
+    let targetY = target.offset().top - header;
+    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+    $('.js-drawer-menu').fadeOut();
+    $('.js-hamburger').removeClass('is-open')
+
+    return false;
+  });
+
+});
+
+
